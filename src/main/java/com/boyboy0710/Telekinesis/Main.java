e6139afb4f0c267e9982835f223b7424a24976d7
boyboy0710 <boyboy0710@naver.com>
2021-02-26 19:34:18 +0900

    1

--- src/main/java/com/boyboy0710/Telekinesis/Main.java
+++ src/main/java/com/boyboy0710/Telekinesis/Main.java
@@ -14,7 +14,7 @@ public final class Main extends JavaPlugin implements Listener {
     public void onEnable() {
         System.out.println("----------------------------------------------------------------------");
         System.out.println("            Telekinesis 를러그인을 성공적으로 불러왔습니다");
-        System.out.println("               플러그인 제작자ㅣ:booyboy0710");
+        System.out.println("               플러그인 제작자:booyboy0710");
         System.out.println("----------------------------------------------------------------------");
     }
 
@@ -25,10 +25,15 @@ public final class Main extends JavaPlugin implements Listener {
     @EventHandler
     public void onInteract(PlayerInteractEvent e) {
         Player p = e.getPlayer();
-        ItemStack item = p.getItemInHand();
         if (e.getAction() == Action.LEFT_CLICK_AIR
                 || e.getAction() == Action.LEFT_CLICK_BLOCK) {
             p.getEyeLocation().getBlock().setType(Material.AIR);
+        }
+        if (e.getAction() == Action.RIGHT_CLICK_AIR
+                || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (p.getEyeLocation().getBlock().getType() == Material.ENDER_CHEST) {
+                p.openInventory(p.getEnderChest());
+            }
         }
     }
 }
